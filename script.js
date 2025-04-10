d4e2f6741de28a80231978ab5ce8e40c106573b4
Sepiow <116896262+Sepiow@users.noreply.github.com>
2025-04-10 13:15:27 +0800

    script commit edited

--- script.js
+++ script.js
@@ -13,14 +13,23 @@ function clearDisplay() {
 
 // Del function to remove last number
 function backspace() {
-  display.value = display.value.slice(0, -1); // slice function
+  display.value = display.value.slice(0, -1); // slice function ng js
 }
 
-// Function to evaluate the mathematical expression entered in the display
+// ung eval js code na talaga na nag cacalculate nung nasa textbox
 function calculate() {
   try {
-    display.value = eval(display.value); // Use JavaScript's eval() to evaluate the expression
+    display.value = eval(display.value); // sabi ng eval aun na ung sa js code na nag cocompute tas ung display value ung nasa text box
   } catch (e) {
     display.value = "Error"; // If an error occurs, display "Error" on the screen
   }
+}
+
+// function ng pang clear on any button press when error is shown
+
+function clearError(value) {
+  if (display.value === "Error") {
+    display.value = ""; // clear the display
+  }
+  display.value += value; // lalagay ung new button press
 }
